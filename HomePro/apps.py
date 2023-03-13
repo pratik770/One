c35f85d5804bd14af7c0265f30b124719aa588b9
pratik770 <70411992+pratik770@users.noreply.github.com>
2023-03-14 02:04:01 +0545

    Add files via upload

---
+++ HomePro/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HomeproConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'HomePro'
---
+++ HomePro/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+from django.contrib import admin
+
+
+urlpatterns = [
+    path("", views.index, name='index'),
+    path('admin/', admin.site.urls),
+
+]
