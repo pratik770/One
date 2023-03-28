2bd8b04f64a5a7b6ea8da8dd0d899375acf08e27
Pratik770 <pratik.piya@hotmail.com>
2023-03-29 04:01:27 +0545

    index

--- One/views.py
+++ One/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import os
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, os.getcwd() +'/templates/index.html')
